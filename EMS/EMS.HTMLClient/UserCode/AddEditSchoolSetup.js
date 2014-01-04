a7823a1bc2ac5521b75d47e0527c8f6fd333b6bf
karthikeyan51 <karthikeyan51@hotmail.com>
2014-01-05 00:11:22 +0530

    Master Complete - Admission

---
+++ EMS/EMS.HTMLClient/UserCode/AddEditSchoolSetup.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditSchoolSetup.Delete_execute = function (screen) {
+    screen.SchoolSetup.deleteEntity();
+    return myapp.commitChanges().then(null, function fail(e) {
+        myapp.cancelChanges();
+        throw e;
+    });
+
+};
