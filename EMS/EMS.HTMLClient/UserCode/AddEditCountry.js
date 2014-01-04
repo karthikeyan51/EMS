467b9108e87e11f5242184ca46a85f62bdec36ab
karthikeyan51 <karthikeyan51@hotmail.com>
2014-01-04 11:41:22 +0530

    Country Screen

---
+++ EMS/EMS.HTMLClient/UserCode/AddEditCountry.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditCountry.Delete_execute = function (screen) {
+    screen.Country.deleteEntity();
+    return myapp.commitChanges().then(null, function fail(e) {
+        myapp.cancelChanges();
+        throw e;
+    });
+};
